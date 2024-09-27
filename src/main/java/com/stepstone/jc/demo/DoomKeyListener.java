76d173203c9ee63ba47a47851cdc66bdf0a34ca8
Thomas Darimont <thomas.darimont@googlemail.com>
2024-09-27 07:57:03 +0200

    Revise ESC handling

--- src/main/java/com/stepstone/jc/demo/DoomKeyListener.java
+++ src/main/java/com/stepstone/jc/demo/DoomKeyListener.java
@@ -16,9 +16,6 @@ class DoomKeyListener extends KeyAdapter {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-            System.exit(0);
-        }
         keyEvents.add(new int[]{0, toDoomKeyCode(e.getKeyCode())});
     }
 
